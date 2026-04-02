e1e6fe67323a1f978d060e8caf07450e01ddb3b8
faciletechnologie375-source <faciletechnologie375@gmail.com>
2026-04-02 02:46:37 +0200

    Set anon key in deployed online config

--- online-config.js
+++ online-config.js
@@ -6,7 +6,7 @@ window.QUIZ_ONLINE_CONFIG = {
   serviceUrl: "https://likdzjbyoicgsdwgsjid.supabase.co",
 
   // Cle "anon" Supabase (Project Settings > API > anon public key)
-  anonKey: "",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxpa2R6amJ5b2ljZ3Nkd2dzamlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwODYwMzAsImV4cCI6MjA5MDY2MjAzMH0.65Vd_DLFoIkwRkv12u8ZMVnxP8OrGjFZuYf5PlA-0j4",
 
   // Nom de la table SQL qui stocke les scores.
   table: "quiz_leaderboard",
